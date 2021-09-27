8118ab3ef5b0b1743651e43998d851ebbf40e748
Guigan713 <guillaume.lequin713@gmail.com>
2021-09-27 18:39:23 +0200

    recreated .env

---
+++ Lundi/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors')
+const morgan = require('morgan')
+const connection = require('./config/db')
+const routes = require('./routes/index')
+
+
+const app = express();
+
+connection.connect(err => {
+    if (err) {
+        console.error('error connecting: ' + err.stack)
+    } else {
+        console.log('database connected')
+    }
+})
+
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(express.urlencoded({ extened: true }))
+
+app.use('/boats', routes.boats)
+app.use('/crews', routes.crews)
+
+app.get('/', (req, res) => {
+    res.status(200).send('je suis dans le /')
+})
+
+
+app.listen(4040, console.log('http://localhost:4040'));
---
+++ Lundi/routes/crews.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const mysql = require('../config/db')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    const sql = 'SELECT * FROM crews'
+    mysql.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).send('error retrieving data from database')
+        } else {
+            console.table(result)
+            res.status(200).json(result)
+        }
+    })
+})
+
+router.get("/:id", (req, res) => {
+	const { id } = req.params
+	const sql = "SELECT * FROM crews WHERE id = ?"
+	const values = [id]
+	mysql.query(sql, values, (err, result) => {
+		if (err) {
+			res.status(500).send("Error retrieving data from database")
+		} else {
+			console.table(result)
+			res.status(200).json(result)
+		}
+	})
+})
+
+router.post("/", (req, res) => {
+	const sql = `INSERT INTO crews (name) VALUES (?)`
+	console.log(req.body)
+	const values = [req.body.name]
+	mysql.query(sql, values, (err, result) => {
+		if (err) {
+			res.status(500).send("Error retrieving data from database")
+		} else {
+			console.table(result)
+			res.status(200).json(result)
+		}
+	})
+})
+
+router.put("/:id", (req, res) => {
+	const { id } = req.params
+	const sql = `UPDATE crews SET name = ? WHERE id = ?`
+	console.log(req.body)
+	const values = [req.body.name, id]
+	mysql.query(sql, values, (err, result) => {
+		if (err) {
+			res.status(500).send("Error retrieving data from database")
+		} else {
+			console.table(result)
+			res.status(200).json(result)
+		}
+	})
+})
+
+router.delete("/:id", (req, res) => {
+	const { id } = req.params
+	const sql = `DELETE FROM crews  WHERE id = ?`
+	const values = [id]
+	mysql.query(sql, values, (err, result) => {
+		if (err) {
+			res.status(500).send("Error retrieving data from database")
+		} else {
+			console.table(result)
+			res.status(200).json(result)
+		}
+	})
+})
+
+module.exports = router
